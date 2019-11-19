7503823550b2d24fbefd14ec67bd032642a99a33
Brandon Ingalls <brandon@t4r.dev>
2019-11-19 04:48:30 +0000

    Added basic empire passthrough

---
+++ files/empire/entrypoint.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+cd '/opt/Empire'
+
+if [ ! -f '/data/.bootstrap' ] ; then
+  rm -f './data/empire.db' ||  { echo '[!] Unable to kill unsecure default db'; exit 1; }
+  ln -sf /data/empire.db ./data/empire.db || { echo '[!] Unable to make db symlink'; exit 1; }
+  ln -sf /data/empire-chain.pem ./data/empire-chain.pem
+  ln -sf /data/empire-priv.key ./data/empire-priv.key
+  STAGING_KEY=RANDOM python './setup/setup_database.py' || { echo '[!] Unable to generate a new db'; exit 1; }
+  { cd ./setup; ./cert.sh || { echo '[!] Unable to setup self signed certs'; exit 1; }; cd ../; }
+  mkdir /data/downloads
+  ln -s /data/downloads
+  touch /data/.bootstrap
+fi
+
+exec -a empire python ./empire
